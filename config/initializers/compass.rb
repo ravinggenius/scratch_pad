1188b70ad6030da9c3f4afaad21a71318e56a1fe
Raving Genius <rg+code@ravinggenius.com>
2010-03-28 19:25:53 -0400

    One more small update

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -2,4 +2,5 @@ require 'compass'
 # If you have any compass plugins, require them here.
 Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
 Compass.configuration.environment = RAILS_ENV.to_sym
+Sass::Plugin.options[:never_update] = true
 Compass.configure_sass_plugin!

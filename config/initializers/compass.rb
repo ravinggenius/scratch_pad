dc19f16c13c8444ec1e59ad5b1a85fc7639e8193
Raving Genius <rg+code@ravinggenius.com>
2010-03-28 20:50:34 -0400

    Fixed some deprication warnings

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 # If you have any compass plugins, require them here.
-Compass.configuration.parse(File.join(RAILS_ROOT, "config", "compass.config"))
-Compass.configuration.environment = RAILS_ENV.to_sym
+Compass.configuration.parse(File.join(Rails.root, "config", "compass.config"))
+Compass.configuration.environment = Rails.env.to_sym
 Sass::Plugin.options[:never_update] = true if Rails.env == 'production'
 Compass.configure_sass_plugin!

a78db78f441d707f997f04aa9e8d2c819f956dbb
Raving Genius <rg+code@ravinggenius.com>
2010-03-28 21:12:00 -0400

    Added some global helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+  def page_id
+    reply = "#{controller_name}_#{action_name}"
+    reply = "#{reply}_#{h(params[:id]).to_i}" if params[:id]
+    reply
+  end
+
+  def page_class
+    "controller_#{controller_name} action_#{action_name}"
+  end
+
+  # http://www.leftrightdesigns.com/library/jquery/nospam/nospam.phps
+  # contact//305online/net
+  #
+  def obfuscate(email_address, options={})
+    email = email_address
+    add_hints = options[:add_hints] || false
+    filter_level = options[:filter_level] || :low
+
+    email.gsub!(/[@]/, add_hints ? '<span class="hint" title="replace with commercial at (@)">//</span>' : '//')
+    email.gsub!(/[\.]/, add_hints ? '<span class="hint" title="replace with period (.)">/</span>' : '/')
+
+    email.reverse! if filter_level == :high
+
+    email
+  end
 end

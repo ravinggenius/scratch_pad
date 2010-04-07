0055bed70c4a8e306474aec4472ef5a52cb708c8
Raving Genius <rg+code@ravinggenius.com>
2010-04-06 21:17:14 -0400

    Updated a bunch of stuff...

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 
 Compass.add_project_configuration(File.join(Rails.root.to_s, 'config', 'compass.rb'))
-Sass::Plugin.options[:never_update] = true if Rails.env == 'production'
+#Sass::Plugin.options[:never_update] = true if Rails.env == 'production'
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+
+user = User.create :name => 'Thomas Ingram', :email => 'thomas@ravinggenius.com'
+
+=begin
+
+List.create :user => user, :title => 'Check out this mighty List!', :items => [ 'Make this work' ]
+
+Table.create :user => user, :title => 'Impressive dataset', :caption => '(It\'s not really that impressive)', :data => '"One","Two"'
+
+Text.create :user => user, :title => 'Welcome', :data => 'Welcome to Raving Genius\' ScratchPad!'
+
+n = Page.create :user => user, :title => 'About Us', :slug => 'about'
+n.nodes << Text.first
+n.nodes << List.first
+n.save
+
+n = Comment.create :user => user, :title => 'My Awesome Comment'
+n.nodes << Text.first
+n.nodes << Table.first
+n.save
+
+=end

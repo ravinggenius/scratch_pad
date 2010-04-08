7f2ca81b01573109a4e39f2fe8d5b3a5ffe7406b
Raving Genius <rg+code@ravinggenius.com>
2010-04-07 22:38:14 -0400

    Updated generator, but still not working

--- lib/generators/node/base.rb
+++ lib/generators/node/base.rb
@@ -3,11 +3,10 @@ require 'rails/generators/named_base'
 module Node
   module Generators
     class Base < Rails::Generators::NamedBase
-      # Automatically sets the source root based on the class name.
-      #
       def self.source_root
-        @_source_root ||= begin
-          File.expand_path(File.join(File.dirname(__FILE__), generator_name, 'templates')) if generator_name
+        #@source_root ||= File.expand_path('../templates', __FILE__)
+        @source_root ||= begin
+          File.expand_path(File.join(Dir.pwd, generator_name, 'templates')) if generator_name
         end
       end
     end

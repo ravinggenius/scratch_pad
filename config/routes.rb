79e56d1faf4eeffa70c9681e9d51f9f590252a04
Raving Genius <rg+code@ravinggenius.com>
2010-04-18 00:16:24 -0400

    Added ideas to routes (commented)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 ScratchPad::Application.routes.draw do |map|
   resources :n, :controller => :nodes, :as => :nodes
 
+  #get ':id' => redirect("nodes/%{id}"), :constraints => { :id => /\d+/ }
+
+  #namespace 'assets' do
+  #  get '/application.css', :to => 'styles#show'
+  #end
+
   root :to => 'nodes#index'
 end

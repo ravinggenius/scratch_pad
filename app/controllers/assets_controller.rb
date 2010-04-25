6be4cd3ed6ff62ff9cd4ed75d327cbea98a4ea54
Raving Genius <rg+code@ravinggenius.com>
2010-04-25 16:09:33 -0400

    Refactored AssetsController#styles

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -7,7 +7,7 @@ class AssetsController < ApplicationController
   # @reference http://groups.google.com/group/haml/browse_thread/thread/e459fbdfa5a6d467/f9ab5f5df3fe77de
   def styles
     @imports = [ 'reset' ]
-    @sass = ''
+    @medias = {}
 
     enabled_extension_styles = {}
 
@@ -18,33 +18,33 @@ class AssetsController < ApplicationController
       end
     end
 
-    template ||= 'default'
-
-    template_styles = extract_media_names(Dir[Rails.root + "lib/templates/#{template}/stylesheets/*"])
-
-    if enabled_extension_styles.key? :all
-      load_extension_styles(:all, enabled_extension_styles[:all])
-      enabled_extension_styles.delete :all
-    end
-
-    if template_styles.include? :all
-      load_template_style(:all, template)
-      template_styles.delete :all
-    end
+    template_name ||= 'default'
+    template_styles = extract_media_names(Dir[Rails.root + "lib/templates/#{template_name}/stylesheets/*"])
 
     enabled_extension_styles.each do |media, extension_styles|
       load_extension_styles(media, extension_styles)
     end
 
     template_styles.each do |media|
-      load_template_style(media, template)
+      load_template_style(media, template_name)
     end
 
-    final_sass = @imports.map do |inc|
-      <<-SASS
+    final_sass = ''
+
+    @imports.each do |inc|
+      final_sass << <<-SASS
 @import '#{inc}'
       SASS
-    end.join + @sass
+    end
+
+    if @medias.key? :all
+      final_sass << extract_styles_for_media(:all, @medias[:all])
+      @medias.delete :all
+    end
+
+    @medias.each do |media, sass|
+      final_sass << extract_styles_for_media(media, sass)
+    end
 
     content_type, body = case params[:format].to_sym
       when :sass then [ :text, final_sass ]
@@ -63,32 +63,34 @@ class AssetsController < ApplicationController
     end.compact
   end
 
-  def clean_media_names media_names
-    media_names.to_s.gsub /_/, ', '
+  def init_media media_names
+    m = media_names.to_s.gsub /_/, ', '
+    @medias[m] ||= ''
+    m
+  end
+
+  def extract_styles_for_media media, sass
+    <<-SASS
+@media #{media}
+#{sass}
+    SASS
   end
 
   def load_extension_styles media, extensions
-    reply = ''
-
-    @sass << <<-SASS
-@media #{clean_media_names media}
-    SASS
-
+    m_key = init_media media
     extensions.each do |extension|
       @imports << "node_extensions/#{extension}/stylesheets/#{media}"
-      @sass << <<-SASS
+      @medias[m_key] << <<-SASS
   .#{extension}
     @include extension_#{extension}_#{media}
       SASS
     end
-
-    reply
   end
 
   def load_template_style media, template
+    m_key = init_media media
     @imports << "templates/#{template}/stylesheets/#{media}"
-    @sass << <<-SASS
-@media #{clean_media_names media}
+    @medias[m_key] << <<-SASS
   @include template_#{template}_#{media}
     SASS
   end

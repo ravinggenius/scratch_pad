a4ef7e0b262477963e61990c0137d8acadfe181a
Raving Genius <rg+code@ravinggenius.com>
2010-04-26 09:53:39 -0400

    Added parenthesis to remove a warning

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 
-Compass.add_project_configuration (Rails.root + 'config' + 'compass.rb').to_s
+Compass.add_project_configuration((Rails.root + 'config' + 'compass.rb').to_s)
 #Sass::Plugin.options[:never_update] = true if Rails.env == 'production'
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!

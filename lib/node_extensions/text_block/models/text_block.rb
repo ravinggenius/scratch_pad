9a9d2ebafbdd108f85a8bb72fc06e195a0e409f0
Raving Genius <rg+code@ravinggenius.com>
2010-04-28 22:32:50 -0400

    Moved node_extensions into separate git submodules

--- lib/node_extensions/text_block/models/text_block.rb
+++
@@ -1,9 +0,0 @@
-class TextBlock
-  include DataMapper::Resource
-  include NodeExtension
-
-  property :id, Serial
-  property :data, Text, :required => true, :lazy => false
-
-  after :save, :set_id
-end

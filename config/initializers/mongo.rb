784cf82ee3c3b40cf72c5a0c021edeaae62a339c
Raving Genius <rg+code@ravinggenius.com>
2010-05-06 23:50:14 -0400

    Updated mongo initializer

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,7 +1,7 @@
-if ENV['MONGOHQ_URL']
-  MongoMapper.config = {Rails.env => {'uri' => ENV['MONGOHQ_URL']}}
-else
-  MongoMapper.config = {Rails.env => {'uri' => 'mongodb://localhost/scratchpad'}}
-end
+MongoMapper.config = {
+  Rails.env => {
+    'uri' => (ENV['MONGOHQ_URL'] ? ENV['MONGOHQ_URL'] : 'mongodb://localhost/scratchpad')
+  }
+}
 
 MongoMapper.connect(Rails.env)

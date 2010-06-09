1a7b80927433f7e8dd1334afee8dfcbf715e234f
Raving Genius <rg+code@ravinggenius.com>
2010-06-08 22:55:35 -0400

    Added some tests for Node

--- test/unit/node_test.rb
+++ test/unit/node_test.rb
@@ -1,8 +1,28 @@
 require 'test_helper'
 
+class TempNode < Node
+end
+
 class NodeTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  setup do
+    @n = TempNode.first_or_create(:title => 'Test Node')
+  end
+
+  test 'expected api' do
+    expected_class_api TempNode, :published
+    expected_instance_api TempNode, :is_published, :name, :name=, :position, :position=, :created_at, :updated_at, :children, :terms, :machine_name
+  end
+
+  test 'aliases' do
+    assert_equal @n.title, @n.name
+    @n.name = 'Another Title'
+    assert_equal @n.name, @n.title
+
+    #assert_equal @n.creator, @n.author
+    #assert_equal @n.updater, @n.editor
+  end
+
+  test 'machine name' do
+    assert_equal 'temp_node', @n.machine_name
   end
 end

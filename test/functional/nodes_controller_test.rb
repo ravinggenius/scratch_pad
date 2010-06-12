01d2793f4366260fc8adb8bc31cb3c783a953e5c
Raving Genius <rg+code@ravinggenius.com>
2010-06-12 11:45:59 -0400

    Cleaned up some functional tests

--- test/functional/nodes_controller_test.rb
+++ test/functional/nodes_controller_test.rb
@@ -5,18 +5,18 @@ class NodesControllerTest < ActionController::TestCase
     @node = nodes(:one)
   end
 
-  test "should get index" do
+  test 'should get index' do
     get :index
     assert_response :success
     assert_not_nil assigns(:nodes)
   end
 
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create node" do
+  test 'should create node' do
     assert_difference('Node.count') do
       post :create, :node => @node.attributes
     end
@@ -24,22 +24,22 @@ class NodesControllerTest < ActionController::TestCase
     assert_redirected_to node_path(assigns(:node))
   end
 
-  test "should show node" do
+  test 'should show node' do
     get :show, :id => @node.to_param
     assert_response :success
   end
 
-  test "should get edit" do
+  test 'should get edit' do
     get :edit, :id => @node.to_param
     assert_response :success
   end
 
-  test "should update node" do
+  test 'should update node' do
     put :update, :id => @node.to_param, :node => @node.attributes
     assert_redirected_to node_path(assigns(:node))
   end
 
-  test "should destroy node" do
+  test 'should destroy node' do
     assert_difference('Node.count', -1) do
       delete :destroy, :id => @node.to_param
     end
--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -5,18 +5,12 @@ class SessionsControllerTest < ActionController::TestCase
     @session = sessions(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:sessions)
-  end
-
-  test "should get new" do
+  test 'should get new' do
     get :new
     assert_response :success
   end
 
-  test "should create session" do
+  test 'should create session' do
     assert_difference('Session.count') do
       post :create, :session => @session.attributes
     end
@@ -24,22 +18,7 @@ class SessionsControllerTest < ActionController::TestCase
     assert_redirected_to session_path(assigns(:session))
   end
 
-  test "should show session" do
-    get :show, :id => @session.to_param
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get :edit, :id => @session.to_param
-    assert_response :success
-  end
-
-  test "should update session" do
-    put :update, :id => @session.to_param, :session => @session.attributes
-    assert_redirected_to session_path(assigns(:session))
-  end
-
-  test "should destroy session" do
+  test 'should destroy session' do
     assert_difference('Session.count', -1) do
       delete :destroy, :id => @session.to_param
     end

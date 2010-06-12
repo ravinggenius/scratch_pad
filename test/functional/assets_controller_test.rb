315d0f1a99d88577dec2605015dc82eb534229f3
Raving Genius <rg+code@ravinggenius.com>
2010-06-12 11:46:45 -0400

    Added some test for AssetsController

--- test/functional/assets_controller_test.rb
+++ test/functional/assets_controller_test.rb
@@ -1,9 +1,27 @@
 require 'test_helper'
 
 class AssetsControllerTest < ActionController::TestCase
-  test "should get styles" do
+  test 'should get scripts' do
+    get :scripts
+    assert_response :success
+  end
+
+  test 'should get styles' do
     get :styles
     assert_response :success
   end
 
+  test 'should have the correct content type set' do
+    get :scripts
+    assert_equal 'application/javascript', response.content_type
+
+    get :styles
+    assert_equal 'text/css', response.content_type
+
+    get :styles, :format => :css
+    assert_equal 'text/css', response.content_type
+
+    get :styles, :format => :sass
+    assert_equal 'text/plain', response.content_type
+  end
 end

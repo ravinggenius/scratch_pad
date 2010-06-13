0df153353c48c0b0cea865b4064b20d34c2e6a48
Raving Genius <rg+code@ravinggenius.com>
2010-06-13 11:48:48 -0400

    Removed some generated code

--- test/functional/sessions_controller_test.rb
+++ test/functional/sessions_controller_test.rb
@@ -2,7 +2,6 @@ require 'test_helper'
 
 class SessionsControllerTest < ActionController::TestCase
   setup do
-    @session = sessions(:one)
   end
 
   test 'should get new' do
@@ -11,18 +10,8 @@ class SessionsControllerTest < ActionController::TestCase
   end
 
   test 'should create session' do
-    assert_difference('Session.count') do
-      post :create, :session => @session.attributes
-    end
-
-    assert_redirected_to session_path(assigns(:session))
   end
 
   test 'should destroy session' do
-    assert_difference('Session.count', -1) do
-      delete :destroy, :id => @session.to_param
-    end
-
-    assert_redirected_to sessions_path
   end
 end

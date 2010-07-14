58886fb57ad2f950c9aa436973d6d32883334d94
Raving Genius <rg+code@ravinggenius.com>
2010-07-13 21:52:35 -0400

    Slight potential tweak to Compass initializer

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,6 @@
 require 'compass'
 
 Compass.add_project_configuration((Rails.root + 'config' + 'compass.rb').to_s)
-#Sass::Plugin.options[:never_update] = true if Rails.env == 'production'
+#Sass::Plugin.options[:never_update] = true
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!

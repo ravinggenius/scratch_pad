356f63aa2abcb5e8c79cabaa405c1b131685e89b
Raving Genius <rg+code@ravinggenius.com>
2010-07-26 19:13:15 -0400

    Queried for all node extensions

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -2,6 +2,12 @@ class Admin::ApplicationController < ApplicationController
   layout 'admin'
 
   before_filter do
-    @active_node_extensions = []
+    @active_node_extensions = available_node_extensions
+  end
+
+  private
+
+  def available_node_extensions
+    @available_node_extensions ||= (Dir.entries Rails.root + 'lib' + 'node_extensions').delete_if { |entry| entry =~ /^\./ }
   end
 end

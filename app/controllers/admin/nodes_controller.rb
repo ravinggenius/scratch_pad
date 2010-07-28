72e4b4b3f0ee0a6d1185561ac50c1d07d880e3a8
Raving Genius <rg+code@ravinggenius.com>
2010-07-28 18:22:05 -0400

    Tried to straighten up Admin::NodesController and views

--- app/controllers/admin/nodes_controller.rb
+++ app/controllers/admin/nodes_controller.rb
@@ -1,15 +1,33 @@
 class Admin::NodesController < Admin::ApplicationController
+  def index
+    @nodes = Node.all
+
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @nodes }
+    end
+  end
+
+  #def show
+  #  @node = node_type.find params[:id]
+
+  #  respond_to do |format|
+  #    format.html
+  #    format.xml  { render :xml => @node }
+  #  end
+  #end
+
   def new
     @node = node_type.new
 
     respond_to do |format|
       format.html
-      format.xml  { render :xml => @node }
+      format.xml  { render :xml => admin_node_url(@node) }
     end
   end
 
   def edit
-    @node = node_type.find(params[:id])
+    @node = node_type.find params[:id]
   end
 
   def create
@@ -17,8 +35,8 @@ class Admin::NodesController < Admin::ApplicationController
 
     respond_to do |format|
       if @node.save
-        format.html { redirect_to(@node, :notice => "#{node_type.name} was successfully created.") }
-        format.xml  { render :xml => @node, :status => :created, :location => @node }
+        format.html { redirect_to(admin_nodes_url, :notice => "#{node_type.name} was successfully created.") }
+        format.xml  { render :xml => @node, :status => :created, :location => node_url(@node) }
       else
         format.html { render :action => 'new' }
         format.xml  { render :xml => @node.errors, :status => :unprocessable_entity }
@@ -27,11 +45,11 @@ class Admin::NodesController < Admin::ApplicationController
   end
 
   def update
-    @node = node_type.find(params[:id])
+    @node = node_type.find params[:id]
 
     respond_to do |format|
       if @node.update_attributes(params[:node])
-        format.html { redirect_to(@node, :notice => "#{node_type.name} was successfully updated.") }
+        format.html { redirect_to(admin_nodes_url, :notice => "#{node_type.name} was successfully updated.") }
         format.xml  { head :ok }
       else
         format.html { render :action => 'edit' }
@@ -41,11 +59,11 @@ class Admin::NodesController < Admin::ApplicationController
   end
 
   def destroy
-    @node = Node.find(params[:id])
+    @node = Node.find params[:id]
     @node.destroy
 
     respond_to do |format|
-      format.html { redirect_to(nodes_url) }
+      format.html { redirect_to admin_nodes_url }
       format.xml  { head :ok }
     end
   end

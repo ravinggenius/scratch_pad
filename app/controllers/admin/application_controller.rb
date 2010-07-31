8053f0c5fdb4521a517ea6dc0231bf7c9dd05fb3
Raving Genius <rg+code@ravinggenius.com>
2010-07-31 17:36:02 -0400

    Added a menu partial

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,6 +5,21 @@ class Admin::ApplicationController < ApplicationController
     @active_node_extensions = available_node_extensions
   end
 
+  before_filter do
+    @main_menu_items = [
+      { :name => 'Dashboard', :href => admin_root_path },
+      { :name => 'New', :href => new_admin_node_path, :children => @active_node_extensions.map { |extension| { :name => extension.titleize, :href => new_admin_node_path(:node_type => extension) } }
+      },
+      { :name => 'Content', :href => admin_nodes_path },
+      { :name => 'Taxonomy', :href => admin_taxonomies_path },
+      { :name => 'Templates', :children => [
+        { :name => 'Frontend' },
+        { :name => 'Backend' }
+      ]},
+      { :name => 'Settings' }
+    ]
+  end
+
   private
 
   def available_node_extensions

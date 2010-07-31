acf8affb432bdefb5c9f967b38dba7e77f921ccc
Raving Genius <rg+code@ravinggenius.com>
2010-07-31 18:00:29 -0400

    Added a brief overview to the dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < Admin::ApplicationController
   def index
+    @node_count = Node.count
+    @vocabulary_count = Vocabulary.count
+    @term_count = Term.count
   end
 end

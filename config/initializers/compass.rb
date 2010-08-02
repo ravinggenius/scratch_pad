c406ef40f028f3bbb29517b37ee8c81ca03599cb
Raving Genius <rg+code@ravinggenius.com>
2010-08-02 13:23:23 -0400

    Removed comment from Compass initializer

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,6 +1,5 @@
 require 'compass'
 
 Compass.add_project_configuration((Rails.root + 'config' + 'compass.rb').to_s)
-#Sass::Plugin.options[:never_update] = true
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!

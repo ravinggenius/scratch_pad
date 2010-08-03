5e028319c0a11ad3c787e1ecc238e171a88e30ad
Raving Genius <rg+code@ravinggenius.com>
2010-08-02 23:02:01 -0400

    Reordered admin menu

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -4,18 +4,19 @@ class Admin::ApplicationController < ApplicationController
   before_filter do
     @main_menu_items = [
       { :name => 'Dashboard', :href => admin_root_path },
-      { :name => 'New', :href => new_admin_node_path, :children => NodeExtension.enabled.map { |extension| { :name => extension.name.titleize, :href => new_admin_node_path(:node_type => extension.name) } } },
-      { :name => 'Content', :href => admin_nodes_path },
+      { :name => 'Content', :href => admin_nodes_path, :children => [
+        { :name => 'New', :href => new_admin_node_path, :children => NodeExtension.enabled.map { |extension| { :name => extension.name.titleize, :href => new_admin_node_path(:node_type => extension.name) } } }
+      ]},
       { :name => 'Taxonomy', :href => admin_vocabularies_path },
+      { :name => 'Widgets' },
       { :name => 'Templates', :children => [
         { :name => 'Frontend', :href => admin_templates_path(:scope => :frontend) },
         { :name => 'Backend', :href => admin_templates_path(:scope => :backend) }
       ]},
-      { :name => 'Widgets' },
-      { :name => 'Users' },
       { :name => 'Filters' },
-      { :name => 'Settings' },
-      { :name => 'Permissions' }
+      { :name => 'Users' },
+      { :name => 'Permissions' },
+      { :name => 'Settings' }
     ]
   end
 end

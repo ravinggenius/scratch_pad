55e2291bef0a5cf4b8c84e458979bd998f2e7ca6
Raving Genius <rg+code@ravinggenius.com>
2010-08-08 13:03:46 -0400

    General code cleanup

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -5,8 +5,8 @@ class Vocabulary
   key :name, String, :required => true
   key :is_freetagging_allowed, Boolean, :required => true, :default => false
   key :may_select_multiple, Boolean, :required => true, :default => true
-  key :node_types_required, Array, :required => true
   key :node_types_optional, Array, :required => true
+  key :node_types_required, Array, :required => true
 
   many :terms
 end

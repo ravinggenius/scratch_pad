86607300569775a4c7524fdc11916de5ed87d6fd
Raving Genius <rg+code@ravinggenius.com>
2010-08-08 22:30:33 -0400

    General code cleanup

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,13 +5,13 @@ module Relationship
   module ClassMethods
     def habtm(this_model, that_model, options = {})
       glue_model = options.has_key?(:glue_model) ? options[:glue_model] : [this_model, that_model].map { |m| Private.single(m).to_s }.sort.join('_')
-      define_method Private::plural(that_model) do
-        @those_models ||= Private::klass(glue_model).send("#{Private::plural(that_model)}_for", self.id)
+      define_method Private.plural(that_model) do
+        @those_models ||= Private.klass(glue_model).send("#{Private.plural(that_model)}_for", self.id)
       end
 
-      define_method "save_#{Private::plural(glue_model)}" do
+      define_method "save_#{Private.plural(glue_model)}" do
         @those_models ||= []
-        @those_models.each { |om| Private::klass(glue_model).first_or_create(Private::id_key(this_model) => self.id, Private::id_key(that_model) => om.id) }
+        @those_models.each { |om| Private.klass(glue_model).first_or_create(Private.id_key(this_model) => self.id, Private.id_key(that_model) => om.id) }
       end
     end
 
@@ -20,19 +20,19 @@ module Relationship
         model_a => model_b,
         model_b => model_a
       }.each do |this_model, that_model|
-        key Private::id_key(this_model), BSON::ObjectID, :required => true
+        key Private.id_key(this_model), BSON::ObjectID, :required => true
 
         define_method this_model do
-          Private::klass(this_model).find(self.send(Private::id_key(this_model)))
+          Private.klass(this_model).find(self.send(Private.id_key(this_model)))
         end
 
         define_method "#{this_model}=" do |new_model|
-          self.send "#{Private::id_key(this_model)}=", new_model.id
+          self.send "#{Private.id_key(this_model)}=", new_model.id
           new_model
         end
 
-        this_model_for = lambda { |that_model_id| all(Private::id_key(that_model) => that_model_id).map { |tagging| tagging.send(this_model) } }
-        self.class.send(:define_method, "#{Private::plural(this_model)}_for", this_model_for)
+        this_model_for = lambda { |that_model_id| all(Private.id_key(that_model) => that_model_id).map { |tagging| tagging.send(this_model) } }
+        self.class.send(:define_method, "#{Private.plural(this_model)}_for", this_model_for)
       end
     end
   end

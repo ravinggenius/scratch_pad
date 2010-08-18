fb02f5255a55bd437737f1f39b668776054d1d5b
Raving Genius <rg+code@ravinggenius.com>
2010-08-17 22:20:42 -0400

    General code cleanup

--- config/initializers/compass.rb
+++ config/initializers/compass.rb
@@ -1,5 +1,5 @@
 require 'compass'
 
-Compass.add_project_configuration((Rails.root + 'config' + 'compass.rb').to_s)
+Compass.add_project_configuration(Rails.root + 'config' + 'compass.rb')
 Compass.configure_sass_plugin!
 Compass.handle_configuration_change!

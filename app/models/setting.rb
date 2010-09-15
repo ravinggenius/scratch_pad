a2fce55a5754c181c9b9e05e59a5031f934d4bad
Raving Genius <rg+code@ravinggenius.com>
2010-09-14 20:34:39 -0400

    Refactored Setting#user_value to be safer to use

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -8,11 +8,9 @@ class Setting
   many :values
 
   def user_value
-    begin
-      values.first :creator_id => User.current.id
-    rescue
-      values.first :creator_id => User.anonymous.id
-    end.value
+    v = values.first :creator_id => User.current.id
+    v = values.first :creator_id => User.anonymous.id if v.nil?
+    v.nil? ? nil : v.value
   end
 
   def self.[](code)

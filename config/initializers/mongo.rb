9870dff95c7ff63b977e5c8aa63ebc8849d41c7a
Raving Genius <rg+code@ravinggenius.com>
2010-09-19 17:01:35 -0400

    Refactored MongoMapper initializer to build MongoDB URI dynamically

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,6 +1,13 @@
+file_name = Rails.root + 'config' + 'database.yml'
+
+if File.exists? file_name
+  y = YAML.load_file(file_name)[Rails.env]
+  mongo_url = "mongodb://#{y['host']}#{y['port'].nil? ? '' : ":#{y['port']}"}/#{y['database']}" # 'ongodb://localhost/scratch_pad
+end
+
 MongoMapper.config = {
   Rails.env => {
-    'uri' => (ENV['MONGOHQ_URL'] ? ENV['MONGOHQ_URL'] : 'mongodb://localhost/scratchpad')
+    'uri' => (ENV['MONGOHQ_URL'] ? ENV['MONGOHQ_URL'] : mongo_url)
   }
 }
 

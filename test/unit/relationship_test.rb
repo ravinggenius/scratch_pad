092e76acbbb5e2258ef1db7b2d00801ca026a3ee
Raving Genius <rg+code@ravinggenius.com>
2010-09-30 01:08:38 -0400

    Fixed a bunch of Relationship tests

--- test/unit/relationship_test.rb
+++ test/unit/relationship_test.rb
@@ -1,24 +1,34 @@
 require 'test_helper'
 
 class ClassA
-  extend Relationship
-  habtm :class_as, :class_bs, :class_glue
+  include MongoMapper::Document
+  include Relationship
+
+  key :name, String
+
+  habtm :class_as, :class_bs, :glue_model => :class_glue
 end
 
 class ClassGlue
-  extend Relationship
+  include MongoMapper::Document
+  include Relationship
+
   habtm_glue :class_a, :class_b
 end
 
 class ClassB
-  extend Relationship
-  habtm :class_bs, :class_as, :class_glue
+  include MongoMapper::Document
+  include Relationship
+
+  key :name, String
+
+  habtm :class_bs, :class_as, :glue_model => :class_glue
 end
 
 class RelationshipTest < ActiveSupport::TestCase
   setup do
-    @a = ClassA.first_or_create
-    @b = ClassB.first_or_create
+    @a = ClassA.first_or_create :name => 'class a'
+    @b = ClassB.first_or_create :name => 'class b'
   end
 
   test 'expected api' do
@@ -26,15 +36,15 @@ class RelationshipTest < ActiveSupport::TestCase
     @a.class_bs << ClassB.new
     @a.class_bs << ClassB.new
     @a.save
-    assert_equal 3, ClassA.first.class_bs.count
+    assert_equal 3, ClassA.first(:name => 'class a').class_bs.count
 
     @b.class_as << ClassA.new
     @b.class_as << ClassA.new
     @b.save
-    assert_equal 2, ClassB.first.class_as.count
+    assert_equal 2, ClassB.first(:name => 'class b').class_as.count
 
-    assert_equal 3, ClassA.class_bs_for(@a.id)
-    assert_equal 2, ClassB.class_as_for(@b.id)
+    assert_equal 3, ClassA.class_bs_for(@a.id).count
+    assert_equal 2, ClassB.class_as_for(@b.id).count
 
     assert_equal 5, ClassGlue.count
   end

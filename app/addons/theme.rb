eab23fadaec390824d080e6aab96bd863ac9968e
Raving Genius <rg+code@ravinggenius.com>
2010-11-09 23:49:25 -0500

    Add Theme settings to Theme.install

--- app/addons/theme.rb
+++ app/addons/theme.rb
@@ -10,4 +10,16 @@ class Theme < AddonBase
   def self.backend
     all.select { |theme| theme.admin? }
   end
+
+  def self.install
+    [
+      { :scope => 'addon.theme.frontend',          :name => 'Frontend Theme',                     :value => :default },
+      { :scope => 'addon.theme.backend',           :name => 'Backend Theme',                      :value => :default_admin },
+      { :scope => 'addon.theme.support.khtml',     :name => 'Experimental Support For KHTML',     :value => false },
+      { :scope => 'addon.theme.support.microsoft', :name => 'Experimental Support For Microsoft', :value => false },
+      { :scope => 'addon.theme.support.mozilla',   :name => 'Experimental Support For Mozilla',   :value => false },
+      { :scope => 'addon.theme.support.opera',     :name => 'Experimental Support For Opera',     :value => false },
+      { :scope => 'addon.theme.support.webkit',    :name => 'Experimental Support For WebKit',    :value => false }
+    ]
+  end
 end

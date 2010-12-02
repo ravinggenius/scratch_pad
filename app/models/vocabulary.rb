f18892d6a78505d95345e542f7170fbed625781f
Raving Genius <rg+code@ravinggenius.com>
2010-12-01 21:53:52 -0500

    Factor Vocabulary#code out

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -1,7 +1,6 @@
 class Vocabulary
   include MongoMapper::Document
 
-  key :code, String, :required => true
   key :name, String, :required => true
   key :is_freetagging_allowed, Boolean, :required => true, :default => false
   key :may_select_multiple, Boolean, :required => true, :default => true

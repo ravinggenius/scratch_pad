3659df7134f22f48e38dbe0752b350ec71b73b61
Raving Genius <rg+code@ravinggenius.com>
2010-12-02 19:01:18 -0500

    Add fields to form and remove edit/new actions/views from Admin::SettingsController

--- app/controllers/admin/settings_controller.rb
+++ app/controllers/admin/settings_controller.rb
@@ -17,31 +17,9 @@ class Admin::SettingsController < Admin::ApplicationController
     end
   end
 
-  def new
-    @setting = Setting.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @setting }
-    end
-  end
-
   def edit
     @setting = Setting.find(params[:id])
-  end
-
-  def create
-    @setting = Setting.new(params[:setting])
-
-    respond_to do |format|
-      if @setting.save
-        format.html { redirect_to(@setting, :notice => 'Setting was successfully created.') }
-        format.xml  { render :xml => @setting, :status => :created, :location => @setting }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @setting.errors, :status => :unprocessable_entity }
-      end
-    end
+    render 'shared/edit_new'
   end
 
   def update
@@ -49,22 +27,12 @@ class Admin::SettingsController < Admin::ApplicationController
 
     respond_to do |format|
       if @setting.update_attributes(params[:setting])
-        format.html { redirect_to(@setting, :notice => 'Setting was successfully updated.') }
+        format.html { redirect_to(admin_settings, :notice => 'Setting was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
         format.xml  { render :xml => @setting.errors, :status => :unprocessable_entity }
       end
-    end
-  end
-
-  def destroy
-    @setting = Setting.find(params[:id])
-    @setting.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(settings_url) }
-      format.xml  { head :ok }
     end
   end
 end

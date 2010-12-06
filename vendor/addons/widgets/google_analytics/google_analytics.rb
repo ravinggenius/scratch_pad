efc93182052cfea4d8a56535eb0352938def12b5
Raving Genius <rg+code@ravinggenius.com>
2010-12-05 22:42:05 -0500

    Make Setting#scope less onerous, part two

--- vendor/addons/widgets/google_analytics/google_analytics.rb
+++ vendor/addons/widgets/google_analytics/google_analytics.rb
@@ -1,6 +1,6 @@
 class GoogleAnalytics < Widget
   def self.enable
-    register_setting 'addon.widget.google_analytics.account_code', 'Account Code', 'UA-xxxxxxx-xx'
+    register_setting 'widget.google_analytics.account_code', 'Account Code', 'UA-xxxxxxx-xx'
     super
   end
 end

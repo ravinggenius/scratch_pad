8a31787f37e63caadc08119692de770dfbcdc070
Raving Genius <rg+code@ravinggenius.com>
2010-12-14 21:23:35 -0500

    Allow Widgets to be associated with regions in Layouts

--- app/controllers/admin/widgets_controller.rb
+++ app/controllers/admin/widgets_controller.rb
@@ -1,6 +1,7 @@
 class Admin::WidgetsController < Admin::ApplicationController
   def index
     @widgets = Widget.enabled
+    @themes = Theme.enabled
 
     respond_to do |format|
       format.html
@@ -9,13 +10,24 @@ class Admin::WidgetsController < Admin::ApplicationController
   end
 
   def update
+    theme = Theme[params[:theme]]
+
+    params[theme.machine_name].each do |layout_name, regions|
+      layout = theme.layouts.first { |l| l.name == layout_name }
+      layout.regions = regions.map do |region_name, widgets|
+        widgets = widgets.reject { |name| name.blank? }.map { |widget_name| Widget[widget_name] }
+        { :name => region_name, :widgets => widgets }
+      end
+      layout.save
+    end
+
     respond_to do |format|
-      if @widget.update_attributes(params[:widget])
-        format.html { redirect_to(@widget, :notice => 'Widget was successfully updated.') }
+      if true
+        format.html { redirect_to(admin_widgets_url, :notice => 'Layout was successfully updated.') }
         format.xml  { head :ok }
       else
         format.html { render :action => admin_widgets_url }
-        format.xml  { render :xml => @widget.errors, :status => :unprocessable_entity }
+        format.xml  { render :xml => [], :status => :unprocessable_entity }
       end
     end
   end

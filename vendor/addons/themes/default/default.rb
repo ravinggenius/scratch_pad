1ea73408f03d752d198fd41668c0886b2f8f1409
Raving Genius <rg+code@ravinggenius.com>
2010-12-16 20:09:27 -0500

    Dasherize layout name

--- vendor/addons/themes/default/default.rb
+++ vendor/addons/themes/default/default.rb
@@ -1,4 +1,4 @@
 class Default < Theme
-  register_layout 'single-column', :branding, :flash, :credits
-  register_layout 'two-column', :branding, :flash, :credits
+  register_layout :single_column, :branding, :flash, :credits
+  register_layout :two_column, :branding, :flash, :credits
 end
--- vendor/addons/themes/default_admin/default_admin.rb
+++ vendor/addons/themes/default_admin/default_admin.rb
@@ -1,3 +1,3 @@
 class DefaultAdmin < Theme
-  register_layout 'single-column', :branding, :flash
+  register_layout :single_column, :branding, :flash
 end

290bf6989f6c62560f59b8acfddad45c1183e879
Raving Genius <rg+code@ravinggenius.com>
2010-12-23 20:55:56 -0500

    Implement Vocabulary#is_hierarchical

--- app/models/vocabulary.rb
+++ app/models/vocabulary.rb
@@ -3,6 +3,7 @@ class Vocabulary
 
   key :name, String, :required => true
   key :is_freetaggable, Boolean, :required => true, :default => false
+  key :is_hierarchical, Boolean, :required => true, :default => false
   key :is_multiselectable, Boolean, :required => true, :default => true
   key :node_types_optional, AddonArray, :required => true
   key :node_types_required, AddonArray, :required => true

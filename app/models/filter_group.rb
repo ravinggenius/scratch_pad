7731b6f91684ae1fb4d9aa5f8d220ca2ce1bbe5a
Raving Genius <rg+code@ravinggenius.com>
2011-01-03 17:39:52 -0500

    Make FilterGroup#name unique

--- app/models/filter_group.rb
+++ app/models/filter_group.rb
@@ -1,7 +1,7 @@
 class FilterGroup
   include MongoMapper::Document 
 
-  key :name, String, :required => true
+  key :name, String, :required => true, :unique => true
   key :filters, AddonArray, :required => true
 
   many :nodes

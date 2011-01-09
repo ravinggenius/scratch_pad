7f5c36f8f6a3c0d120c28c7cf34dcdbdfc8850e8
Raving Genius <rg+code@ravinggenius.com>
2011-01-08 23:01:45 -0500

    Remove unused second layout from Default theme

--- vendor/addons/themes/default/default.rb
+++ vendor/addons/themes/default/default.rb
@@ -6,12 +6,4 @@ class Default < Theme
     regions << register_region(:content, :section)
     regions << register_region(:credits, :footer)
   end
-
-  register_layout :two_column do |regions|
-    regions << register_region(:branding, :header)
-    regions << register_region(:main_menu, :nav)
-    regions << register_region(:flash, :section)
-    regions << register_region(:content, :section)
-    regions << register_region(:credits, :footer)
-  end
 end

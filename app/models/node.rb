ravinggenius/scratch_pad

commit cd1700aaabf5e34fbd4cfd6b446873e87c4eccf0

Formalize Node#state

app/models/node.rb
@@ -2,6 +2,11 @@
 # http://railstips.org/blog/archives/2010/06/16/mongomapper-08-goodies-galore/
 
 class Node
+  STATES = [
+    :draft,
+    :published
+  ]
+
   attr_writer :children
 
   include MongoMapper::Document
@@ -10,7 +15,7 @@ class Node
   key :path, String
   key :filter_group_id, BSON::ObjectId, :required => true
   key :children_ids, Array, :typecast => 'BSON::ObjectId'
-  key :state, String, :required => true, :default => :draft # TODO formalize states
+  key :state, String, :required => true, :default => :draft
   key :title, String, :required => true
 
   timestamps!
@@ -23,6 +28,7 @@ class Node
 
   validate :ensure_not_ancestor_of_self
   validate :ensure_unique_path_if_present
+  validate :ensure_valid_state
 
   def children
     (self.children_ids || []).map { |child_id| Node.find child_id }.reject &:blank?
@@ -103,6 +109,11 @@ class Node
       paths = Node.all.reject { |n| n.id == id }.map(&:path).reject(&:blank?)
       errors.add :path, 'must be unique or blank' if paths.include?(path)
     end
+  end
+
+  def ensure_valid_state
+    states = self.class::STATES.map &:to_s
+    errors.add :state, "must be one of #{states.join ', '}" unless states.include state.to_s
   end
 
   # TODO move to before_save &block

app/controllers/admin/nodes_controller.rb
@@ -104,6 +104,8 @@ class Admin::NodesController < Admin::ApplicationController
       end.sort.insert 0, ['Node', 'node']
     end
 
+    @node_states = Node::STATES.map { |state| [state, state] }
+
     @filter_groups = FilterGroup.all.sort_by &:name
 
     set_vocabulary_ivars

app/views/admin/nodes/_form.html.haml
@@ -11,6 +11,9 @@
     %div.field
       = form.label :filter_group_id, 'Filter Group'
       = form.collection_select :filter_group_id, @filter_groups, :id, :name, :selected => @node.filter_group.id
+    %div.field
+      = form.label :state, 'State'
+      = form.select :state, @node_states, :selected => @node.state
     - if @node.new?
       %div.field
         = label_tag :node_type, 'Node Type'

commit b7a9dbb908ef44065e26d75009d0a67044bc4d3e

Fix bug introduced in commit cd1700a

app/models/node.rb
@@ -113,7 +113,7 @@ class Node
 
   def ensure_valid_state
     states = self.class::STATES.map &:to_s
-    errors.add :state, "must be one of #{states.join ', '}" unless states.include state.to_s
+    errors.add :state, "must be one of #{states.join ', '}" unless states.include? state.to_s
   end
 
   # TODO move to before_save &block
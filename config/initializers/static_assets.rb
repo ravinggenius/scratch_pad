6990e7850a97cb9ff861a023645b3337606aaf75
Raving Genius <rg+code@ravinggenius.com>
2011-03-06 15:46:15 -0500

    Fix bug where ./tmp/assets may not exist

--- config/initializers/static_assets.rb
+++ config/initializers/static_assets.rb
@@ -1,5 +1,6 @@
 # remove all children in case an asset was updated since our last deploy
-(Rails.root + 'tmp' + 'assets').children.each do |child|
+asset_dir = (Rails.root + 'tmp' + 'assets')
+asset_dir.children.each do |child|
   next if child.basename.to_s.starts_with? '.'
   FileUtils.rm_r child
-end
+end if asset_dir.directory?

3bc41e5e1e1b0ddd6debc5a6872fd168675c5e21
Raving Genius <rg+code@ravinggenius.com>
2011-03-27 20:34:56 -0400

    Add FireSASS support (not working)

--- config/compass.rb
+++ config/compass.rb
@@ -2,6 +2,10 @@ project_type = :rails
 project_path = Compass::AppIntegration::Rails.root
 environment = Compass::AppIntegration::Rails.env
 
+# support for FireSASS
+# TODO find out why this doesn't seem to work
+sass_options = { :debug_info => true }
+
 output_style = (Rails.env.production? ? :compressed : :expanded)
 preferred_syntax = :sass
 relative_assets = false

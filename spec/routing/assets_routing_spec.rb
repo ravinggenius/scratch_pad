de4a6e1d7ab52737a85df37bda1bc4ad06bb83d1
Raving Genius <rg+code@ravinggenius.com>
2011-04-06 00:20:17 -0400

    Fix routing specs

--- spec/routing/assets_routing_spec.rb
+++ spec/routing/assets_routing_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe AssetsController do
   describe 'routing' do
+    it 'recognizes and generates #scripts' do
+      { :get => '/assets/theme/default/scripts.js' }.should route_to(:controller => 'assets', :action => 'scripts', :addon_type => 'theme', :addon => 'default', :format => 'js')
+    end
+
+    it 'recognizes and generates #styles' do
+      { :get => '/assets/theme/default/styles.css' }.should route_to(:controller => 'assets', :action => 'styles', :addon_type => 'theme', :addon => 'default', :format => 'css')
+      { :get => '/assets/theme/default/styles.sass' }.should route_to(:controller => 'assets', :action => 'styles', :addon_type => 'theme', :addon => 'default', :format => 'sass')
+    end
+
     it 'recognizes and generates #static' do
       { :get => '/assets/theme/default/fonts/times.woff' }.should route_to(:controller => 'assets', :action => 'static', :addon_type => 'theme', :addon => 'default', :asset_type => 'fonts', :asset_name => 'times.woff')
       { :get => '/assets/theme/default/images/background.png' }.should route_to(:controller => 'assets', :action => 'static', :addon_type => 'theme', :addon => 'default', :asset_type => 'images', :asset_name => 'background.png')
@@ -10,15 +19,6 @@ describe AssetsController do
 
     it 'recognizes and generates #routes' do
       { :get => '/assets/routes' }.should route_to(:controller => 'assets', :action => 'routes')
-    end
-
-    it 'recognizes and generates #scripts' do
-      { :get => '/assets/default/scripts.js' }.should route_to(:controller => 'assets', :action => 'scripts', :theme => 'default', :format => 'js')
-    end
-
-    it 'recognizes and generates #styles' do
-      { :get => '/assets/default/styles.css' }.should route_to(:controller => 'assets', :action => 'styles', :theme => 'default', :format => 'css')
-      { :get => '/assets/default/styles.sass' }.should route_to(:controller => 'assets', :action => 'styles', :theme => 'default', :format => 'sass')
     end
   end
 end

1d75fd5bcae8a28593b03592dfb1f6af804e78de
Raving Genius <rg+code@ravinggenius.com>
2011-05-01 22:59:09 -0400

    Stop requiring Layout#is_default as it has a default value specified

--- app/models/layout.rb
+++ app/models/layout.rb
@@ -3,7 +3,7 @@ class Layout
 
   key :theme, String, :required => true
   key :name, String, :required => true
-  key :is_default, Boolean, :required => true, :default => false
+  key :is_default, Boolean, :default => false
 
   many :regions, :class_name => 'LayoutRegion'
 

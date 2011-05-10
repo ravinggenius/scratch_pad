c054c99f0a3c2f774269e466b4afa6577a835fb9
Raving Genius <rg+code@ravinggenius.com>
2011-05-09 20:05:26 -0400

    Specify custom view for home page by leaving Setting[site.home_page] blank

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,32 +1,38 @@
 class HomeController < ApplicationController
   # TODO convert this to a lambda and move to routes, perhaps
   def index
-    setting = Setting[:site, :home_page].split '#'
+    home_page = Setting[:site, :home_page]
 
-    case setting.count
-    when 1
-      # show a specific node
-      params[:path] = setting.first
-      s_controller, s_action = :nodes, :show_human
-    when 2, 3
-      # show an arbitrary controller#action pair, with optional id
-      s_controller, s_action, params[:id] = setting
-      params.delete :id unless params[:id]
+    if home_page.present?
+      setting = home_page.split '#'
+
+      case setting.count
+      when 1
+        # show a specific node
+        params[:path] = setting.first
+        s_controller, s_action = :nodes, :show_human
+      when 2, 3
+        # show an arbitrary controller#action pair, with optional id
+        s_controller, s_action, params[:id] = setting
+        params.delete :id unless params[:id]
+      else
+        # this shouldn't happen...
+        # fall back to just listing nodes
+        s_controller, s_action = :nodes, :index
+      end
+
+      reply = "#{s_controller}_controller".classify.constantize.action(s_action).call(env)
+      status, response_headers, response_body = reply
+
+      # FIXME this is gross, but seems to work
+      # this is needed because status is 304 every other request
+      if (200..299).include? status
+        render :text => response_body.body
+      else
+        head status
+      end
     else
-      # this shouldn't happen...
-      # fall back to just listing nodes
-      s_controller, s_action = :nodes, :index
-    end
-
-    reply = "#{s_controller}_controller".classify.constantize.action(s_action).call(env)
-    status, response_headers, response_body = reply
-
-    # FIXME this is gross, but seems to work
-    # this is needed because status is 304 every other request
-    if (200..299).include? status
-      render :text => response_body.body
-    else
-      head status
+      render :file => @selected_theme.find_view(:home)
     end
   end
 end

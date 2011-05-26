e405da0dc0b4558a5737a6f095f80640957e1e86
Raving Genius <rg+code@ravinggenius.com>
2011-05-26 14:18:17 -0400

    Remove support for non-gem addons

--- config/initializers/addons.rb
+++ config/initializers/addons.rb
@@ -1,13 +1,4 @@
-# activate gem addons (preferred method)
 addons = Gem.loaded_specs.keys.map { |g| g.scan /^scratch_pad-(.*)/; $1 }.compact.each do |addon|
   scope, name = addon.split '-'
   require "scratch_pad-#{scope}-#{name}/#{name}"
-end
-
-# activate old-style addons
-base = Rails.root + 'vendor' + 'addons'
-Dir[base + '**' + '*.rb'].each do |name|
-  name =~ /^#{Regexp.escape(base.to_s)}\/(.*)\.rb$/
-  require base + $1
-  ActiveSupport::Deprecation.warn "Addon (#{name}) should be implemented as a gem."
 end
